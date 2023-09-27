b9a3240a87b0724357c1fe4d2e9856fefa40d339
joona95 <joona95@naver.com>
2023-09-27 20:57:47 +0900

    feat: 메소드 이름으로 쿼리 생성

--- src/main/java/com/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.study.datajpa.repository;
 import com.study.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
