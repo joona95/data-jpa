9ce335968ea2e8f82cb148339f1c116b51e1faf2
joona95 <joona95@naver.com>
2023-09-29 08:06:26 +0900

    feat: Web 확장 - 페이징과 정렬

--- src/main/java/com/study/datajpa/dto/MemberDto.java
+++ src/main/java/com/study/datajpa/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.study.datajpa.dto;
 
+import com.study.datajpa.entity.Member;
 import lombok.Data;
 
 @Data
@@ -13,5 +14,10 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
